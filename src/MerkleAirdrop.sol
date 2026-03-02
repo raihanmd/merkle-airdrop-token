b3e26cd34610f28ce74be57c73d3ed299ee4e2bd
raihanmd <mraihanf471@gmail.com>
2026-03-02 10:10:31 +0700

    feat: add claim with signature, for rich choice wether with v,r,s or just signature

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.30;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MerkleAirdrop {
+contract MerkleAirdrop is EIP712 {
   using SafeERC20 for IERC20;
 
   error MerkleAirdrop__InvalidProof();
+  error MerkleAirdrop__InvalidSignature();
   error MerkleAirdrop__AlreadyClaimed();
 
   event Claimed(address indexed account, uint256 amount);
@@ -17,14 +20,27 @@ contract MerkleAirdrop {
 
   mapping(address => bool) private s_claimed;
 
-  constructor(address token, bytes32 merkleRoot) {
+  bytes32 private constant CLAIM_TYPEHASH = keccak256("Claim(address account,uint256 amount)");
+
+  struct Claim {
+    address account;
+    uint256 amount;
+  }
+
+  constructor(address token, bytes32 merkleRoot) EIP712("MerkleAirdrop", "1") {
     i_token = IERC20(token);
     i_merkleRoot = merkleRoot;
   }
 
-  function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+  function claim(address account, uint256 amount, bytes32[] calldata merkleProof, uint8 v, bytes32 r, bytes32 s)
+    external
+  {
     if (s_claimed[account]) {
       revert MerkleAirdrop__AlreadyClaimed();
+    }
+
+    if (!_verifySignature(account, getMessageHash(account, amount), v, r, s)) {
+      revert MerkleAirdrop__InvalidSignature();
     }
 
     // Leaf node = account + amount
@@ -38,6 +54,51 @@ contract MerkleAirdrop {
 
     emit Claimed(account, amount);
     i_token.safeTransfer(account, amount);
+  }
+
+  function claim(address account, uint256 amount, bytes32[] calldata merkleProof, bytes memory signature)
+    external
+  {
+    if (s_claimed[account]) {
+      revert MerkleAirdrop__AlreadyClaimed();
+    }
+
+    if (!_verifySignature(account, getMessageHash(account, amount), signature)) {
+      revert MerkleAirdrop__InvalidSignature();
+    }
+
+    // Leaf node = account + amount
+    bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+
+    if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+      revert MerkleAirdrop__InvalidProof();
+    }
+
+    s_claimed[account] = true;
+
+    emit Claimed(account, amount);
+    i_token.safeTransfer(account, amount);
+  }
+
+  function getMessageHash(address account, uint256 amount) public view returns (bytes32) {
+    return _hashTypedDataV4(keccak256(abi.encode(CLAIM_TYPEHASH, Claim({account: account, amount: amount}))));
+  }
+
+  // ====================
+  // Internal functions
+  // ====================
+  function _verifySignature(address account, bytes32 digest, uint8 v, bytes32 r, bytes32 s)
+    internal
+    pure
+    returns (bool)
+  {
+    (address signer,,) = ECDSA.tryRecover(digest, v, r, s);
+    return signer == account;
+  }
+
+  function _verifySignature(address account, bytes32 digest, bytes memory signature) internal pure returns (bool) {
+    (address signer,,) = ECDSA.tryRecover(digest, signature);
+    return signer == account;
   }
 
   // ====================
--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -14,25 +14,45 @@ contract MerkleAirdropTest is Test {
 
   uint256 amount = 25e18; // 25 tokens with 18 decimals
 
-  address account1 = address(0x6CA6d1e2D5347Bfab1d91e883F1915560e09129D);
-  address account2 = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
-  address account3 = address(0x2ea3970Ed82D5b30be821FAAD4a731D35964F7dd);
-  address account4 = address(0x08Dc514b8bA9015a74972Da8bDa5027fD91943e1);
+  address account1;
+  uint256 account1PrivKey;
+
+  address account2 = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266); // gas payer
 
   bytes32 proof1One = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
   bytes32 proof1Two = 0x74aded9f47912765423c6564320da18a960d56a893263dde9696d12d8b3a7421;
   bytes32[] proof1 = [proof1One, proof1Two];
 
   function setUp() public {
+    (account1, account1PrivKey) = makeAddrAndKey("user");
+
     (airdrop, token) = new MerkleAirdropDeploy().run();
 
     token.mint(address(airdrop), amount * 4);
   }
 
-  function test_claim() public {
+  function test_claim_vrs() public {
     uint256 initialBalance = token.balanceOf(account1);
 
-    airdrop.claim(account1, amount, proof1);
+    bytes32 digest = airdrop.getMessageHash(account1, amount);
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(account1PrivKey, digest);
+
+    vm.prank(account2);
+    airdrop.claim(account1, amount, proof1, v, r, s);
+
+    assertEq(token.balanceOf(account1), initialBalance + amount);
+  }
+
+  function test_claim_signature() public {
+    uint256 initialBalance = token.balanceOf(account1);
+
+    bytes32 digest = airdrop.getMessageHash(account1, amount);
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(account1PrivKey, digest);
+
+    vm.prank(account2);
+    airdrop.claim(account1, amount, proof1, bytes.concat(r, s, bytes1(v)));
 
     assertEq(token.balanceOf(account1), initialBalance + amount);
   }
